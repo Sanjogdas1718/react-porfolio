2e6e1baab89801a4400088ee0576846fae2d9c79
Sanjog Das <96170131+Sanjogdas1718@users.noreply.github.com>
2024-12-12 11:14:28 +0000

    fourth commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -66,7 +66,7 @@ export const Navbar = () => {
             <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact">Contacts</a>
           </li>
         </ul>
       </div>
